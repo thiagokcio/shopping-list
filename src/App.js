cf5986dc44362d13dd6a0b9cccd13ce28ea81d0c
Thiago Maia <thiagokcio@hotmal.com>
2024-02-16 14:45:12 -0300

    initial version app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ShoppingList from './components/ShoppingList/ShoppingList';
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center p-5 bg-slate-600">
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
